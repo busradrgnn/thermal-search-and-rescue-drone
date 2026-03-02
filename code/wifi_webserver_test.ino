454925bf481242b9d90f2cdfe656ae61d7be96e3
Büşra <198649006+busradrgnn@users.noreply.github.com>
2026-03-02 23:33:59 +0300

    Add WiFi web server test code
    Set up a basic HTTP server to display thermal data.

--- code/wifi_webserver_test.ino
+++ code/wifi_webserver_test.ino
@@ -1,1 +1,25 @@
+//Stage 1 - Creating HTTP server
+#include <WiFi.h>
+#include <WebServer.h>
 
+const char* ssid = "WiFi_ADI";
+const char* password = "WiFi_SIFRESI";
+
+WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "Termal veriler burada gösterilecek");
+}
+
+void setup() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+  server.on("/", handleRoot);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}

aad3b21471acfaa2033f7c3bd9189d90d25fc9e8
Büşra <198649006+busradrgnn@users.noreply.github.com>
2026-03-02 23:38:37 +0300

    Add basic WiFi server test for ESP32
    Implement a basic WiFi server using ESP32 that responds to HTTP requests.

---
+++ code/http_server_test.ino
@@ -0,0 +1,26 @@
+//Stage 2 - Basic wifi server test
+#include <WiFi.h> 
+#include <WebServer.h>
+
+const char* ssid = "DSMART_0AF326";
+const char* password = "AichYqz4";
+
+WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "ESP32 Calisiyor!");
+}
+
+void setup() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+  
+  server.on("/", handleRoot);  // Ana sayfaya bir yanıt ekle
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();  // HTTP isteklerini işle
+}
